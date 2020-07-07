9e02a79c04e43414741ac3c88d84b981b51073ed
sujith <sujithsuresh246@gmail.com>
2020-07-07 15:03:54 +0530

    app.js modified

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor = "white" />
+      <Text style={styles.textColor}>__Write__.</Text>
+      <TextInput style={styles.textField} value={'search'}/>
+      <View style={styles.boxWrapper}> 
+
+      </View>
     </View>
   );
 }
@@ -14,8 +19,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+    
+    color: 'white',
   },
+  textColor: {
+    color: 'white',
+    fontSize: 24,
+    marginVertical: 30,
+  },
+  textField : {
+    height: 40, 
+    color: '#858585',
+    backgroundColor: '#262626', 
+    width: '100%', 
+    borderWidth: 1,
+    borderRadius: 5,
+    textAlign: 'center',
+  },
+  boxWrapper: { 
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  }
 });
